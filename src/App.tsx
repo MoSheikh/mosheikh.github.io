3705db50483fada074f320226cc2d0a87cf06ea2
mo <mosheikh1996@gmail.com>
2024-02-14 19:58:48 -0600

    Customize

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 
 export default function Page() {
@@ -16,18 +17,21 @@ export default function Page() {
       "Are you sure?",
       "What if I asked really nicely?",
       "Pretty please",
+      "Wtf :/",
       "With a chocolate rice cake on top",
       "What about a matcha frostie",
       "PLEASE POOKIE",
       "But :*(",
       "I am going to die",
       "Yep im dead",
-      "ok ur talking to nathan's ghost",
+      "ok ur talking to mo's ghost",
       "please babe",
       ":((((",
       "PRETTY PLEASE",
       "Estoy muerto",
-      "No :(",
+      "Je suis mort",
+      "Call me daddy :(",
+      "Baby :(",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -38,7 +42,9 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">
+            WOOOOOO!!! I love you pookie!! ;))
+          </div>
         </>
       ) : (
         <>
